baa01e14b95bbd6d690502591b57d0e4c4695f68
avdhut400 <avdhutmagar@gmail.com>
2026-02-25 23:39:27 +0530

    i

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -64,7 +64,10 @@ const loginUser = async (req, res, next) => {
 const logoutUser = (req, res) => {
   res.cookie("token", "", {
     httpOnly: true,
-    expires: new Date(0)
+    secure: true,
+    sameSite: "none",
+    path: "/",
+    expires: new Date(0),
   });
 
   res.status(200).json({ message: "Logged out successfully" });
